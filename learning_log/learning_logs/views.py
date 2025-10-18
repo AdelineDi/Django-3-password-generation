bab8a80c336c1ffbcad4414443671f34793edbe1
Darya Zenina <dariadasha13@gmail.com>
2025-10-18 21:51:40 +0300

    update

---
+++ learning_log/learning_logs/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, redirect
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+def index(request):
+    """Домашняя страница прилодения learning Logs"""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Выводит список тем"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Выводит одну тему и все ее записи"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Определяет новую тему"""
+    if request.method != 'POST':
+        form = TopicForm()
+    #Данные не отправлялись,создается пустая форма
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+        #Отправлены даннык POST; Обработать данные
+
+    #Вывести пустую или недействительную форму
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Добавляет новую запись по конкретной теме"""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """Редактирует существующую запись"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
+
+
